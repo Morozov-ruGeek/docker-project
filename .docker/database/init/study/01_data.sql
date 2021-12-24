e6fd922cd14e6fc0a8bdc701ad5706c5a772f842
Aleksei Morozov <aleksei_morozov@epam.com>
2021-12-24 14:43:43 +0400

    Add spring-cloud-config

--- .docker/database/init/study/01_data.sql
+++ .docker/database/init/study/01_data.sql
@@ -1,16 +1,16 @@
-insert into STUDENTS(id, first_name, last_name, start_date)
+INSERT INTO STUDENTS(id, first_name, last_name, start_date)
 VALUES (1, 'Aleksei', 'Morozov', '2021-10-01'),
        (2, 'Aleksandra', 'Morozova', '2021-11-15'),
        (3, 'Vladislav', 'Morozov', '2021-11-01'),
        (4, 'Vladimir', 'Krasnosolnishko', '2021-10-01'),
        (5, 'Ivan', 'Grozny', '2021-10-15');
 
-insert into COURSES(id, NAME, amount)
-values (1, 'materials_science', 200.0),
+INSERT INTO COURSES(id, NAME, amount)
+VALUES (1, 'materials_science', 200.0),
        (2, 'probability_theory', 300.0);
 
-insert into TOPICS(id, name, hours_durations)
-values (1, 'structure', 6),
+INSERT INTO TOPICS(id, name, hours_durations)
+VALUES (1, 'structure', 6),
        (2, 'specifications', 8),
        (3, 'manufacture', 12),
        (4, 'thermodynamics', 14),
@@ -20,8 +20,8 @@ values (1, 'structure', 6),
        (8, 'total_probability_formula_and_Bayes_formula', 4),
        (9, 'poisson_formula', 4);
 
-insert into SCORES(id, topic_id, student_id, score)
-values (1, 1, 1, 70),
+INSERT INTO SCORES(id, topic_id, student_id, score)
+VALUES (1, 1, 1, 70),
        (2, 2, 1, 80),
        (3, 3, 1, 90),
        (4, 4, 1, 100),
@@ -31,14 +31,14 @@ values (1, 1, 1, 70),
        (8, 8, 4, 50),
        (9, 9, 4, 30);
 
-insert into students_courses(student_id, course_id)
-values (1, 1),
+INSERT INTO students_courses(student_id, course_id)
+VALUES (1, 1),
        (2, 1),
        (3, 1),
        (4, 2);
 
-insert into course_topics(course_id, topic_id)
-values (1, 1),
+INSERT INTO course_topics(course_id, topic_id)
+VALUES (1, 1),
        (1, 2),
        (1, 3),
        (1, 4),
@@ -48,18 +48,18 @@ values (1, 1),
        (2, 8),
        (2, 9);
 
-insert into roles (name)
-values ('ROLE_STUDENT'),
+INSERT INTO roles (name)
+VALUES ('ROLE_STUDENT'),
        ('ROLE_TEACHER');
 
-insert into users (username, password)
-values ('student', '$2a$12$rGWj2mBSHptwtooswQ.BReuho90.JP9KEIA93bv.4swdmK4hs0rHq'),
+INSERT INTO users (username, password)
+VALUES ('student', '$2a$12$rGWj2mBSHptwtooswQ.BReuho90.JP9KEIA93bv.4swdmK4hs0rHq'),
        ('teacher', '$2a$12$rGWj2mBSHptwtooswQ.BReuho90.JP9KEIA93bv.4swdmK4hs0rHq');
 
-insert into users_roles (user_id, role_id)
-values (1, 1),
+INSERT INTO users_roles (user_id, role_id)
+VALUES (1, 1),
        (2, 2);
 
-insert into payments(id, student_id, course_id)
-values (1, 1, 1),
+INSERT INTO payments(id, student_id, course_id)
+VALUES (1, 1, 1),
        (2, 4, 2);
