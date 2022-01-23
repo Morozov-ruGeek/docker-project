d96a6b882148a400638ea0d56ea454aac9b06c0a
Aleksei Morozov <aleksei_morozov@epam.com>
2022-01-23 22:10:39 +0400

    Added lgging and traceId

--- study-center/src/main/java/com/epam/amorozov/studycenter/configs/JdbcConfig.java
+++ study-center/src/main/java/com/epam/amorozov/studycenter/configs/JdbcConfig.java
@@ -1,5 +1,6 @@
 package com.epam.amorozov.studycenter.configs;
 
+import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
